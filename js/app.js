124935ce47eed5ecc6a69639005beb64d50ac741
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-04 17:54:08 -0600

    create basic board, input, and keyboard elements in html and css

---
+++ js/app.js
@@ -0,0 +1,2 @@
+// Import word-bank object
+// Create a random generator function to select a word that will be used for each round of the game
---
+++ js/word-bank.js
@@ -0,0 +1,2 @@
+// Create a word bank to pull from as an object
+// Export it
